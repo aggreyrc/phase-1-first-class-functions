eed43f94cdacdf5bf35c37e96558b5e4705f75f5
aggreyrc <aggreyronoh@gmail.com>
2024-06-27 14:48:03 +0300

    First class function

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback();;
+}
+
+function returnsANamedFunction(){
+    function myFuntion(){}
+        return myFuntion;
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
